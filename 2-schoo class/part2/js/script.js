2a2f530ea5f217cd3cc62241eadf0ed15c562553
Elle Kasai <elle.kasai@gmail.com>
2014-03-24 01:56:02 +0900

    fix main

--- 2-schoo class/part2/js/script.js
+++ 2-schoo class/part2/js/script.js
@@ -2,26 +2,26 @@ $(function() {
   // ----------------------------------------------------------
   // When you're lecturing, do this part last.
   var contribution = {
-    "youbi-1-day-5": "c",
-    "youbi-1-day-10": "cc",
-    "youbi-2-day-8": "c",
-    "youbi-2-day-18": "cc",
-    "youbi-3-day-11": "c",
-    "youbi-3-day-22": "cc",
-    "youbi-4-day-19": "c",
-    "youbi-4-day-30": "cc",
-    "youbi-5-day-6": "c",
-    "youbi-5-day-8": "cc",
-    "youbi-6-day-12": "c",
-    "youbi-6-day-44": "cc",
-    "youbi-7-day-11": "c",
-    "youbi-7-day-50": "cc"
+    "youbi-1-day-5": "log-yellow-green",
+    "youbi-1-day-10": "log-green",
+    "youbi-2-day-8": "log-yellow-green",
+    "youbi-2-day-18": "log-green",
+    "youbi-3-day-11": "log-yellow-green",
+    "youbi-3-day-22": "log-green",
+    "youbi-4-day-19": "log-yellow-green",
+    "youbi-4-day-30": "log-green",
+    "youbi-5-day-6": "log-yellow-green",
+    "youbi-5-day-8": "log-green",
+    "youbi-6-day-12": "log-yellow-green",
+    "youbi-6-day-44": "log-green",
+    "youbi-7-day-11": "log-yellow-green",
+    "youbi-7-day-50": "log-green"
   };
   // ----------------------------------------------------------
 
   for (var youbi = 1; youbi <= 7; youbi++) {
     for (var day = 1; day <= 53; day++) {
-      var box = $("<span class='box'></span>");
+      var box = $("<span class='main-box-log-square'></span>");
 
       // ----------------------------------------------------------
       // When you're lecturing, do this part last.
